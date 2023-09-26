981ccc5bf302dd6cecb5bc28a8f2b415f15cda5d
ersanjaysah <ssah6694@gmail.com>
2023-09-26 21:51:21 +0530

    changed file name

--- terraform-serverfile/main.tf
+++ terraform-serverfile/main.tf
@@ -10,16 +10,16 @@ resource "aws_instance" "test-server" {
             host = self.public_ip
         }
         provisioner "remote-exec" {
-            inline = [ "echo 'wait to start the instance' " ]
+            inline = ["echo'wait to start the instance'"]
         }
         tags = {
             Name= "BankingTest-Server"
         }
         provisioner "local-exec" {
-            command = " echo ${aws_instance.test-server.public_ip} > inventory "
+            command = "echo ${aws_instance.test-server.public_ip} > inventory"
         }
         provisioner "local-exec" {
-            command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-serverfile/ansible_playbook.yml"
+            command ="ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-serverfile/playbook.yml"
         }
-  
+
 }
