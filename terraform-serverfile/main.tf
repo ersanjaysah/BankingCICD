a9486e7f456921c197d945a0e8856ed3ab092eed
ersanjaysah <ssah6694@gmail.com>
2023-09-26 22:21:48 +0530

    m

--- terraform-serverfile/main.tf
+++ terraform-serverfile/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "test-server1" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
   key_name               = "insure-key"
@@ -7,16 +7,16 @@ resource "aws_instance" "test-server" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("./insure-key.pem")
-    host        = self.public_ip
+    host        = self.private_ip
   }
   provisioner "remote-exec" {
-    inline = ["echo'wait to start the instance'"]
+    inline = ["echo 'wait to start the instance' "]
   }
   tags = {
     Name = "BankingTest-Server"
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+    command = " echo ${aws_instance.test-server1.private_ip} > inventory "
   }
   provisioner "local-exec" {
     command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-serverfile/playbook.yml"
