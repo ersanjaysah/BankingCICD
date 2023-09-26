578828562d4b83c9f13151c3c09c02e7e4ec5c9c
ersanjaysah <ssah6694@gmail.com>
2023-09-26 22:05:54 +0530

    fi

--- terraform-serverfile/main.tf
+++ terraform-serverfile/main.tf
@@ -1,25 +1,25 @@
 resource "aws_instance" "test-server" {
-        ami = "ami-0f5ee92e2d63afc18"
-        instance_type = "t2.micro"
-        key_name = "insure-key"
-        vpc_security_group_ids = [ "sg-06f4f0628632ed8fe" ]
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-            private_key = file("./insure-key.pem")
-            host = self.public_ip
-        }
-        provisioner "remote-exec" {
-            inline = ["echo'wait to start the instance'"]
-        }
-        tags = {
-            Name= "BankingTest-Server"
-        }
-        provisioner "local-exec" {
-            command = "echo ${aws_instance.test-server.public_ip} > inventory"
-        }
-        provisioner "local-exec" {
-            command ="ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-serverfile/playbook.yml"
-        }
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = "insure-key"
+  vpc_security_group_ids = ["sg-06f4f0628632ed8fe"]
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./insure-key.pem")
+    host        = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = ["echo'wait to start the instance'"]
+  }
+  tags = {
+    Name = "BankingTest-Server"
+  }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-serverfile/playbook.yml"
+  }
 
 }
